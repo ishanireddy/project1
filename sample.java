165478fcd1cc27e25fa24f0bd463a638e5db400f
ishani <ishanigujjula@gmail.com>
2024-12-09 14:40:55 +0530

    sample.java content ishani gujjula

--- sample.java
+++ sample.java
@@ -2,18 +2,6 @@ public class Sample
 {
 	public static void main(String[]args)
 	{
-	    Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
-
-        int sum = 0;
-
-        while (num != 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-
-        System.out.println("Sum of digits: " + sum);
-    }
+	    System.out.println("ishani gujjula");
+	}
 }
